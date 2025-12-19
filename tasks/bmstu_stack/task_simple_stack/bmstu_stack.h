3cfdc233b5ba4c7cd66d1b417f93a34d3f6b1b1f
dmitriikononov <dmitriikononov@dmitriikononovs-MacBook-Pro.local>
2025-12-20 02:07:07 +0300

    Проходит тесты на SimpleString

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -6,82 +6,99 @@
 
 namespace bmstu
 {
-template <typename T>
-class stack
-{
-   public:
-	stack() : data_(nullptr), size_(0u) {}
+    template <typename T> class stack
+    {
+      public:
+        stack() : data_(nullptr), size_(0u) {}
 
-	bool empty() const noexcept { return size_ == 0u; }
-
-	size_t size() const noexcept { return size_; }
-
-	~stack() {
-        clear();
-        ::operator delete[](data_);
-    }
-
-	template <typename... Args>
-	void emplace(Args&&... args)
-	{
-        grow();
-        new (data_ + size_) T(std::forward<Args>(args)...);
-        ++size_;
-
-	}
-
-	void push(T&& value) {
-        grow();
-        new(data_ + size_) T(std::move(value));
-        ++size_;
-    }
-
-	void clear() noexcept {
-        for (size_t i = 0; i < size_; ++i) {
-            data_[i].~T();
+        bool empty() const noexcept
+        {
+            return size_ == 0u;
         }
-        size_ = 0;
-    }
 
-	void push(const T& value) {
-        grow();
-        new(data_ + size_) T(value);
-        ++size_;
-    }
-
-	void pop() {
-        if (empty()) { throw std::underflow_error("stack is empty"); }
-        data_[size_-1].~T();
-        --size_;
-    }
-
-	T& top() { 
-        if (empty()) {
-            throw std::underflow_error("stack is empty");
+        size_t size() const noexcept
+        {
+            return size_;
         }
-        return data_[size_ - 1];
-    }
 
-	const T& top() const {
-        if (empty()) {
-            throw std::underflow_error("stack is empty");
+        ~stack()
+        {
+            clear();
+            ::operator delete[](data_);
         }
-        return data_[size_ - 1];
-    }
 
-   private:
+        template <typename... Args> void emplace(Args&&... args)
+        {
+            grow();
+            new (data_ + size_) T(std::forward<Args>(args)...);
+            ++size_;
+        }
 
-   void grow() {
-    const size_t new_size = size_ + 1;
-    T* new_data = (T*)::operator new[](new_size * sizeof(T));
-    for (size_t i = 0; i < size_; ++i) {
-        new (new_data + i) T(std::move(data_[i]));
-        data_[i].~T();
-    }
-    ::operator delete[](data_);
-    data_ = new_data;
-   }
-	T* data_;
-	size_t size_;
-};
-}  // namespace bmstu
+        void push(T&& value)
+        {
+            grow();
+            new (data_ + size_) T(std::move(value));
+            ++size_;
+        }
+
+        void clear() noexcept
+        {
+            for (size_t i = 0; i < size_; ++i)
+            {
+                data_[i].~T();
+            }
+            size_ = 0;
+        }
+
+        void push(const T& value)
+        {
+            grow();
+            new (data_ + size_) T(value);
+            ++size_;
+        }
+
+        void pop()
+        {
+            if (empty())
+            {
+                throw std::underflow_error("stack is empty");
+            }
+            data_[size_ - 1].~T();
+            --size_;
+        }
+
+        T& top()
+        {
+            if (empty())
+            {
+                throw std::underflow_error("stack is empty");
+            }
+            return data_[size_ - 1];
+        }
+
+        const T& top() const
+        {
+            if (empty())
+            {
+                throw std::underflow_error("stack is empty");
+            }
+            return data_[size_ - 1];
+        }
+
+      private:
+        void grow()
+        {
+            const size_t new_size = size_ + 1;
+            T* new_data = (T*)::operator new[](new_size * sizeof(T));
+            for (size_t i = 0; i < size_; ++i)
+            {
+                new (new_data + i) T(std::move(data_[i]));
+                data_[i].~T();
+            }
+            ::operator delete[](data_);
+            data_ = new_data;
+        }
+        T* data_;
+        size_t size_;
+    };
+} // namespace bmstu
